9e75555032bf6576a87709ede2540ad71e626e28
OzBirdog <ozbirdett@gmail.com>
2023-11-12 00:31:22 -0600

    Added Compare

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ with open("complete.json", "r") as json_file:
 # Convert the JSON data to a DataFrame
 df = pd.DataFrame(data)
 
-# Streamlit UI
-st.title("Scrape My Professor")
+compare = 0
+
+col1, col2 = st.columns(2)
+
 container = st.container()
 
 rating = 0.0
@@ -23,11 +25,16 @@ formattedDifficulty = "N/A"
 again = 0.0
 formattedAgain = "N/A"
 
+
 def colored_box(value, color):
     return f'<div style="background-color:{color}; padding:10px; border-radius:10px; max-width: 80px; text-align: center;">{value}</div>'
 
 def rateMyProfessor(professor_name):
     professor = ratemyprofessor.get_professor_by_school_and_name(ratemyprofessor.get_school_by_name("University of North Texas"), professor_name)
+    if compare == 1:
+        st.title(f"Professor {professor_name}")
+    else:    
+        st.title(f"Professor {professor_name} of the %s Department." % (professor.department))
     if professor is None:
         st.text("Professor Not Found!")
     else:    
@@ -44,29 +51,29 @@ def rateMyProfessor(professor_name):
             again = float(professor.would_take_again)
             formattedAgain = f"{round(professor.would_take_again, 1)}%"
 
-        container.header("Rating")
+        st.header("Rating")
         if rating < 2.0:
-            container.markdown(colored_box(formattedRating, 'red'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedRating, 'red'), unsafe_allow_html=True)
         elif 2.0 < rating < 4.0:
-            container.markdown(colored_box(formattedRating, 'yellow'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedRating, 'yellow'), unsafe_allow_html=True)
         elif rating >= 4.0:
-            container.markdown(colored_box(formattedRating, 'green'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedRating, 'green'), unsafe_allow_html=True)
 
-        container.header("Difficulty")
+        st.header("Difficulty")
         if difficulty < 2.0:
-            container.markdown(colored_box(formattedDifficulty, 'green'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedDifficulty, 'green'), unsafe_allow_html=True)
         elif 2.0 < difficulty < 4.0:
-            container.markdown(colored_box(formattedDifficulty, 'yellow'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedDifficulty, 'yellow'), unsafe_allow_html=True)
         elif difficulty >= 4.0:
-            container.markdown(colored_box(formattedDifficulty, 'red'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedDifficulty, 'red'), unsafe_allow_html=True)
 
-        container.header("Would Take Again")
+        st.header("Would Take Again")
         if again < 50:
-            container.markdown(colored_box(formattedAgain, 'red'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedAgain, 'red'), unsafe_allow_html=True)
         elif 50 < again < 70:
-            container.markdown(colored_box(formattedAgain, 'yellow'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedAgain, 'yellow'), unsafe_allow_html=True)
         elif again >= 70:
-            container.markdown(colored_box(formattedAgain, 'green'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedAgain, 'green'), unsafe_allow_html=True)
 
 def gradeJSON(professor_name, class_name):
     # Filter the DataFrame for the specific professor and class
@@ -103,16 +110,46 @@ def gradeJSON(professor_name, class_name):
                 st.write("No matching results found for the specified professor and class.")
 
 st.sidebar.header("Search for a Professor or Class")
-professor_name = st.sidebar.text_input("Professor Name")
-class_name = st.sidebar.text_input("Class Name")
-if st.sidebar.button("Search"):
-    if professor_name:
-        rateMyProfessor(professor_name)
-        if not class_name:
-            st.subheader(f"Grades for Professor {professor_name}")
-        else:
-            st.subheader(f"Grades for Professor {professor_name} and {class_name}")    
-        gradeJSON(professor_name, class_name)
-    elif class_name:
-        st.subheader(f"Grades for {class_name}")
-        gradeJSON(professor_name, class_name)    
+if st.sidebar.checkbox("Compare"):
+    professor_name2 = st.sidebar.text_input("(Additional) Professor Name")
+    class_name2 = st.sidebar.text_input("(Additional) Class Name")
+    professor_name = st.sidebar.text_input("Professor Name")
+    class_name = st.sidebar.text_input("Class Name")
+    if st.sidebar.button("Compare"):
+        compare = 1
+        with col1:
+            if professor_name:
+                rateMyProfessor(professor_name)
+                if not class_name:
+                    st.subheader(f"Grades for Professor {professor_name}")
+                else:
+                    st.subheader(f"Grades for {class_name} by Professor {professor_name}")    
+                gradeJSON(professor_name, class_name)
+            elif class_name:
+                st.subheader(f"Grades for {class_name}")
+                gradeJSON(professor_name, class_name)   
+        with col2:         
+            if professor_name2:
+                rateMyProfessor(professor_name2)
+                if not class_name2:
+                    st.subheader(f"Grades for Professor {professor_name2}")
+                else:
+                    st.subheader(f"Grades for {class_name2} by Professor {professor_name2}")    
+                gradeJSON(professor_name2, class_name2)
+            elif class_name2:
+                st.subheader(f"Grades for {class_name2}")
+                gradeJSON(professor_name2, class_name2)     
+else:    
+    professor_name = st.sidebar.text_input("Professor Name")
+    class_name = st.sidebar.text_input("Class Name")
+    if st.sidebar.button("Search"):
+        if professor_name:
+            rateMyProfessor(professor_name)
+            if not class_name:
+                st.subheader(f"Grades for Professor {professor_name}")
+            else:
+                st.subheader(f"Grades for {class_name} by Professor {professor_name}")    
+            gradeJSON(professor_name, class_name)
+        elif class_name:
+            st.subheader(f"Grades for {class_name}")
+            gradeJSON(professor_name, class_name)    

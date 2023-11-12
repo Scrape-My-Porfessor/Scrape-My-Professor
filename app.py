934d73070361b1f65c528dc6fe6f414254fb6435
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-11-12 02:08:16 -0600

    changed colors

--- app.py
+++ app.py
@@ -63,27 +63,27 @@ def rateMyProfessor(professor_name):
 
         st.header("Rating")
         if rating < 2.0:
-            st.markdown(colored_box(formattedRating, 'red'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedRating, '#eb2d3a'), unsafe_allow_html=True)
         elif 2.0 < rating < 4.0:
-            st.markdown(colored_box(formattedRating, 'yellow'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedRating, '#ffec00'), unsafe_allow_html=True)
         elif rating >= 4.0:
-            st.markdown(colored_box(formattedRating, 'green'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedRating, '#50b458'), unsafe_allow_html=True)
 
         st.header("Difficulty")
         if difficulty < 2.0:
-            st.markdown(colored_box(formattedDifficulty, 'green'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedDifficulty, '#50b458'), unsafe_allow_html=True)
         elif 2.0 < difficulty < 4.0:
-            st.markdown(colored_box(formattedDifficulty, 'yellow'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedDifficulty, '#ffec00'), unsafe_allow_html=True)
         elif difficulty >= 4.0:
-            st.markdown(colored_box(formattedDifficulty, 'red'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedDifficulty, '#eb2d3a'), unsafe_allow_html=True)
 
         st.header("Would Take Again")
         if again < 50:
-            st.markdown(colored_box(formattedAgain, 'red'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedAgain, '#eb2d3a'), unsafe_allow_html=True)
         elif 50 < again < 70:
-            st.markdown(colored_box(formattedAgain, 'yellow'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedAgain, '#ffec00'), unsafe_allow_html=True)
         elif again >= 70:
-            st.markdown(colored_box(formattedAgain, 'green'), unsafe_allow_html=True)
+            st.markdown(colored_box(formattedAgain, '#50b458'), unsafe_allow_html=True)
 
 def gradeJSON(professor_name, class_name):
     # Filter the DataFrame for the specific professor and class
@@ -117,7 +117,7 @@ def gradeJSON(professor_name, class_name):
             df_combined, x='Grade', y='Count',
             color='Grade',
             labels={'Count': 'Count', 'Grade': 'Grades'},
-            color_discrete_map={'A': 'green', 'B': 'yellow', 'C': 'orange', 'D': 'maroon', 'F': 'red', 'W': 'grey'},
+            color_discrete_map={'A': '#50b458', 'B': '#ffec00', 'C': 'orange', 'D': 'maroon', 'F': '#eb2d3a', 'W': 'grey'},
         )
         if compare == 0:
             fig.update_layout(width=700)
@@ -178,9 +178,9 @@ def pi(professor_name, class_name):
                             "radius": "70%",
                             "top": "40%",
                             "data": [
-                                {"value": pass_count, "name": "Pass", "itemStyle": {"color": "green"}},
-                                {"value": fail_count, "name": "Fail", "itemStyle": {"color": "red"}},
-                                {"value": drop_count, "name": "Drop", "itemStyle": {"color": "yellow"}},
+                                {"value": pass_count, "name": "Pass", "itemStyle": {"color": "#50b458"}},
+                                {"value": fail_count, "name": "Fail", "itemStyle": {"color": "#eb2d3a"}},
+                                {"value": drop_count, "name": "Drop", "itemStyle": {"color": "#ffec00"}},
                             ],
                             "emphasis": {
                                 "itemStyle": {

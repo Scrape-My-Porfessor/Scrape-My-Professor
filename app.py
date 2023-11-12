892c0106a5ad66db09bc997ac2e3d50ccf61e947
OzBirdog <ozbirdett@gmail.com>
2023-11-12 01:24:03 -0600

    Added Title Screen

--- app.py
+++ app.py
@@ -11,8 +11,10 @@ with open("complete.json", "r") as json_file:
 df = pd.DataFrame(data)
 
 compare = 0
+title = 0
 
 col1, col2 = st.columns(2)
+col3, col4 = st.columns([0.6, 0.4])
 
 container = st.container()
 
@@ -25,19 +27,26 @@ formattedDifficulty = "N/A"
 again = 0.0
 formattedAgain = "N/A"
 
+title_style = 'text-align: center;'
+header_style = 'text-align: center; color: green;'
+
 
 def colored_box(value, color):
     return f'<div style="background-color:{color}; padding:10px; border-radius:10px; max-width: 80px; text-align: center;">{value}</div>'
 
+
+def checkFields(name1, name2):
+    return name1.strip().lower() == name2.strip().lower() if name1 and name2 else False
+
 def rateMyProfessor(professor_name):
     professor = ratemyprofessor.get_professor_by_school_and_name(ratemyprofessor.get_school_by_name("University of North Texas"), professor_name)
-    if compare == 1:
-        st.title(f"Professor {professor_name}")
-    else:    
-        st.title(f"Professor {professor_name} of the %s Department." % (professor.department))
     if professor is None:
-        st.text("Professor Not Found!")
-    else:    
+        st.title("Professor Not Found!")
+    else: 
+        if compare == 1:
+            st.title(f"Professor {professor_name}")
+        else:    
+            st.title(f"Professor {professor_name} of the %s Department." % (professor.department))   
         rating = float(professor.rating)
         formattedRating = "%.1f / 5.0" % professor.rating
 
@@ -107,7 +116,7 @@ def gradeJSON(professor_name, class_name):
                 }
                 st_echarts(options=options, height="500px")
             else:
-                st.write("No matching results found for the specified professor and class.")
+                st.title("No matching results found for the specified professor and class.")
 
 def pi(professor_name, class_name):
             ##########################################Pie Chart#################################################
@@ -132,97 +141,116 @@ def pi(professor_name, class_name):
                         if grade in combined_grades:
                             combined_grades[grade] += int(count)
 
-            pass_fail_drop = {
-                "Pass": ["A", "B", "C"],
-                "Fail": ["D", "F"],
-                "Drop": ["W"]
-            }
-            pass_count = fail_count = drop_count = 0
-            #Combines Pass/Fail/Drop grades
-            for grade, count in combined_grades.items():
-                if grade in pass_fail_drop["Pass"]:
-                    pass_count += count
-                elif grade in pass_fail_drop["Fail"]:
-                    fail_count += count
-                elif grade in pass_fail_drop["Drop"]:
-                    drop_count += count
-            #Create the pie chart
-            options = {
-                "title": {"text": "Pass/Fail/Drop", "left": "center"},
-                "tooltip": {"trigger": "item"},
-                "legend": {"orient": "vertical", "left": "left"},
-                "series": [
-                    {
-                        "name": "Pass/Fail/Drop",
-                        "type": "pie",
-                        "radius": "50%",
-                        "data": [
-                            {"value": pass_count, "name": "Pass", "itemStyle": {"color": "green"}},
-                            {"value": fail_count, "name": "Fail", "itemStyle": {"color": "red"}},
-                            {"value": drop_count, "name": "Drop", "itemStyle": {"color": "yellow"}},
-                        ],
-                        "emphasis": {
-                            "itemStyle": {
-                                "shadowBlur": 10,
-                                "shadowOffsetX": 0,
-                                "shadowColor": "rgba(0, 0, 0, 0.5)",
-                            }
-                        },
-                    }
-                ],
-            }
+                pass_fail_drop = {
+                    "Pass": ["A", "B", "C"],
+                    "Fail": ["D", "F"],
+                    "Drop": ["W"]
+                }
+                pass_count = fail_count = drop_count = 0
+                #Combines Pass/Fail/Drop grades
+                for grade, count in combined_grades.items():
+                    if grade in pass_fail_drop["Pass"]:
+                        pass_count += count
+                    elif grade in pass_fail_drop["Fail"]:
+                        fail_count += count
+                    elif grade in pass_fail_drop["Drop"]:
+                        drop_count += count
+                #Create the pie chart
+                options = {
+                    "title": {"text": "Pass/Fail/Drop", "left": "center"},
+                    "tooltip": {"trigger": "item"},
+                    "legend": {"orient": "vertical", "left": "left"},
+                    "series": [
+                        {
+                            "name": "Pass/Fail/Drop",
+                            "type": "pie",
+                            "radius": "50%",
+                            "data": [
+                                {"value": pass_count, "name": "Pass", "itemStyle": {"color": "green"}},
+                                {"value": fail_count, "name": "Fail", "itemStyle": {"color": "red"}},
+                                {"value": drop_count, "name": "Drop", "itemStyle": {"color": "yellow"}},
+                            ],
+                            "emphasis": {
+                                "itemStyle": {
+                                    "shadowBlur": 10,
+                                    "shadowOffsetX": 0,
+                                    "shadowColor": "rgba(0, 0, 0, 0.5)",
+                                }
+                            },
+                        }
+                    ],
+                }
 
-            # Render the pie chart
-            st_echarts(
-                options=options, height="600px",
-            )
+                # Render the pie chart
+                st_echarts(
+                    options=options, height="600px",
+                )
+            else:
+                st.write("")
+
+
+if title == 0:
+    st.markdown('<h1 style="{}">Welcome to Scrape My Professor</h1>'.format(title_style), unsafe_allow_html=True)
+    st.markdown('<h2 style="{}">(UNT Edition)</h2>'.format(header_style), unsafe_allow_html=True)
+    st.markdown('<h1 style="{}">Please Enter Professor or Class Name</h1>'.format(title_style), unsafe_allow_html=True)           
+
 st.sidebar.header("Search for a Professor or Class")
 if st.sidebar.checkbox("Compare"):
-    professor_name2 = st.sidebar.text_input("(Additional) Professor Name")
-    class_name2 = st.sidebar.text_input("(Additional) Class Name")
-    professor_name = st.sidebar.text_input("Professor Name")
-    class_name = st.sidebar.text_input("Class Name")
+    professor_name2 = st.sidebar.text_input("(Additional) Professor Name", placeholder = "First Last")
+    class_name2 = st.sidebar.text_input("(Additional) Class Name", placeholder = "Class Name")
+    professor_name = st.sidebar.text_input("Professor Name", placeholder = "First Last")
+    class_name = st.sidebar.text_input("Class Name", placeholder = "Class Name")
     if st.sidebar.button("Compare"):
-        compare = 1
-        with col1:
-            if professor_name:
-                rateMyProfessor(professor_name)
-                if not class_name:
-                    st.subheader(f"Grades for Professor {professor_name}")
-                else:
-                    st.subheader(f"Grades for {class_name} by Professor {professor_name}")    
-                gradeJSON(professor_name, class_name)
-                pi(professor_name, class_name)
-            elif class_name:
-                st.subheader(f"Grades for {class_name}")
-                gradeJSON(professor_name, class_name)   
-                pi(professor_name, class_name) 
-        with col2:         
-            if professor_name2:
-                rateMyProfessor(professor_name2)
-                if not class_name2:
-                    st.subheader(f"Grades for Professor {professor_name2}")
-                else:
-                    st.subheader(f"Grades for {class_name2} by Professor {professor_name2}")    
-                gradeJSON(professor_name2, class_name2)
-                pi(professor_name2, class_name2)
-            elif class_name2:
-                st.subheader(f"Grades for {class_name2}")
-                gradeJSON(professor_name2, class_name2)  
-                pi(professor_name2, class_name2)   
+        title = 1
+        if checkFields(professor_name, professor_name2):
+            st.text("Fields are the same!")
+        elif checkFields(class_name, class_name2):
+            st.text("Fields are the same!")
+        else:    
+            compare = 1
+            with col1:
+                if professor_name:
+                    rateMyProfessor(professor_name)
+                    if not class_name:
+                        st.subheader(f"Grades for Professor {professor_name}")
+                    else:
+                        st.subheader(f"Grades for {class_name} by Professor {professor_name}")    
+                    gradeJSON(professor_name, class_name)
+                    pi(professor_name, class_name)
+                elif class_name:
+                    st.subheader(f"Grades for {class_name}")
+                    gradeJSON(professor_name, class_name)   
+                    pi(professor_name, class_name) 
+            with col2:         
+                if professor_name2:
+                    rateMyProfessor(professor_name2)
+                    if not class_name2:
+                        st.subheader(f"Grades for Professor {professor_name2}")
+                    else:
+                        st.subheader(f"Grades for {class_name2} by Professor {professor_name2}")    
+                    gradeJSON(professor_name2, class_name2)
+                    pi(professor_name2, class_name2)
+                elif class_name2:
+                    st.subheader(f"Grades for {class_name2}")
+                    gradeJSON(professor_name2, class_name2)  
+                    pi(professor_name2, class_name2)   
 else:    
-    professor_name = st.sidebar.text_input("Professor Name")
-    class_name = st.sidebar.text_input("Class Name")
+    professor_name = st.sidebar.text_input("Professor Name", placeholder = "First Last")
+    class_name = st.sidebar.text_input("Class Name", placeholder = "Class Name")
     if st.sidebar.button("Search"):
+        title = 1
         if professor_name:
-            rateMyProfessor(professor_name)
+            with col3:
+                rateMyProfessor(professor_name)
             if not class_name:
                 st.subheader(f"Grades for Professor {professor_name}")
             else:
                 st.subheader(f"Grades for {class_name} by Professor {professor_name}")    
             gradeJSON(professor_name, class_name)
-            pi(professor_name, class_name)
+            with col4:
+                pi(professor_name, class_name)
         elif class_name:
             st.subheader(f"Grades for {class_name}")
-            gradeJSON(professor_name, class_name)  
-            pi(professor_name, class_name)  
+            gradeJSON(professor_name, class_name)
+            with col4:  
+                pi(professor_name, class_name)  
